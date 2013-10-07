5d44f8bf1dbdb0b493b16675dc61c7367ea55a1a
Christoph Helma <helma@in-silico.ch>
2013-10-07 14:10:05 +0200

    song mode working

---
+++ test.rb
@@ -0,0 +1,36 @@
+require_relative "seq.rb"
+
+at_exit do
+  nibbler = Nibbler.new
+  puts nibbler.parse("00 B0 7B 00").inspect
+  #puts nibbler.parse("B07B00").inspect
+end
+
+@input = UniMIDI::Input.first.open
+
+mbase = Instrument.new 1,9
+brute = Instrument.new 0,15
+mm3 = Instrument.new 0,2
+
+#bass = Loop.new 0, 15, ["C2","G3","E3","C4"], [1,1,0,1,0,0]
+#kick = Loop.new 1, 9, "C2",  [1,0,0,0]
+#snr =  Loop.new 0, 2, "E4",  [0,0,0,0,1,0,0,0]
+#hh =   Loop.new 0, 2, "D#4", [0,0,1,0]
+bass = Loop.new brute, ["C2","G3","E3","C4"], [1,1,0,1,0,0]
+kick = Loop.new mbase, "C2",  [1,0,0,0]
+snr =  Loop.new mm3, "E4",  [0,0,0,0,1,0,0,0]
+hh =   Loop.new mm3, "D#4", [0,0,1,0]
+
+#=begin
+score = [
+  [2,  [kick]],
+  [2, [kick,bass]],
+  [8, [kick,bass,hh]],
+  [4,  [bass,hh]]
+]
+#=end
+#score = [ [32,[kick,snr,hh ]]]
+
+song = Song.new 132, score
+song.loop 8, 8
+song.start
