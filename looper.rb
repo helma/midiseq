2ab1b55bba582ed6f727d42d7b0470da07cc72c8
Christoph Helma <helma@in-silico.ch>
2015-12-07 12:43:47 +0100

    new looper version

--- looper.rb
+++ looper.rb
@@ -1,8 +1,10 @@
+=begin
+require "unimidi"
 require "micromidi"
 require "topaz"
 
 class Timer < Topaz::Tempo
-  attr_reader :counter, :clockin
+  attr_reader :counter, :clockin, :length
   def initialize
     @clockin = UniMIDI::Input.use 0
     @counter = 0
@@ -13,6 +15,7 @@ class Timer < Topaz::Tempo
       @dcounter = (@dcounter + 1) % (2*@length)
       @counter = @dcounter/2
     end
+    start :background => true
   end
   def rec_counter # quantize recordings
     (@dcounter+1)/2
@@ -21,21 +24,25 @@ end
 
 class Recorder 
 
-  attr_reader @sequence
+  attr_reader :sequence, :timer
   def initialize timer
     @timer = timer
-    @sequence = Array.new @length
+    @sequence = Array.new @timer.length
     @midiin =  UniMIDI::Input.use 1
+    #Topaz::Tempo.new @timer.clockin, :interval => 16 do
+      #@sequence[@timer.counter] = nil
+    #end
     recorder = MIDIEye::Listener.new(@midiin)
     recorder.listen_for(:class => [MIDIMessage::NoteOn, MIDIMessage::NoteOff]) do |event|
-      if event.class == MIDIMessage::NoteOn
-        @opennotes[event[:message].note] = [event[:message], @timer.rec_counter]
-      elsif event.class == MIDIMessage::NoteOff
-        onmessage = @opennotes[event[:message].note].first
-        start = @opennotes[event[:message].note].last
-        dur = @timer.rec_counter - start
-        @sequence[start] = [onmessage.note, onmessage.velocity, dur]
-      end
+        puts event.inspect
+#      if event.class == MIDIMessage::NoteOn
+#        @opennotes[event[:message].note] = [event[:message], @timer.rec_counter]
+#      elsif event.class == MIDIMessage::NoteOff
+#        onmessage = @opennotes[event[:message].note].first
+#        start = @opennotes[event[:message].note].last
+#        dur = @timer.rec_counter - start
+#        @sequence[start] = [onmessage.note, onmessage.velocity, dur]
+#      end
     end
     recorder.run(:background => true)
   end
@@ -48,13 +55,17 @@ class Player  < Topaz::Tempo
     @recorder = recorder
     @timer = recorder.timer
     @midiout =  UniMIDI::Output.use 1
-    @midiout.channel 15
     @sequence = recorder.sequence
     super @timer.clockin, :interval => 16 do
       pid = fork do
         step = @sequence[@timer.counter]
-        @midiout.velocity step[1]
-        @midiout.play step[0], step[2]*60/@timer.tempo/4
+        if step
+          MIDI.using(@midiout) do
+            channel 15
+            velocity step[1]
+            play step[0], step[2]*60/@timer.tempo/4
+          end
+        end
       end
       Process.detach pid
     end
@@ -64,10 +75,63 @@ class Player  < Topaz::Tempo
     @sequence = recorder.sequence
   end
 end
+=end
 
-player = Player.new(Recorder.new(Timer.new))
-sleep 2
-player.start
-sleep 4
-player.stop
+#t = Timer.new
+#r = Recorder.new t
+#loop do
+  #sleep 2
+  #puts t.tempo
+  #puts r.sequence.inspect
+#end
+#player = Player.new(Recorder.new(Timer.new))
+#sleep 2
+#player.start
+#sleep 4
+#player.stop
 
+    #@timer = t
+    #@sequence = Array.new @timer.length
+    #recorder = MIDIEye::Listener.new(@midiin)
+    #recorder.listen_for(:class => [MIDIMessage::NoteOn, MIDIMessage::NoteOff]) do |event|
+    #recorder.listen do |event|
+        #puts event.inspect #unless even.class == MIDIMessage::ControlChange
+=begin
+      if event.class == MIDIMessage::NoteOn
+        @opennotes[event[:message].note] = [event[:message], @timer.rec_counter]
+      elsif event.class == MIDIMessage::NoteOff
+        onmessage = @opennotes[event[:message].note].first
+        start = @opennotes[event[:message].note].last
+        dur = @timer.rec_counter - start
+        @sequence[start] = [onmessage.note, onmessage.velocity, dur]
+      end
+=end
+    #end
+    #recorder.run#(:background => true)
+#@midiin =  UniMIDI::Input.use 1
+=begin
+AlsaRawMIDI::Input.last.open do |input|
+loop do
+  sleep 0.01
+  data = input.gets
+  puts data #unless data.first[:data].first == 191 ## cc chan 16
+  #puts data.inspect
+end
+=end
+
+require "rtmidi"
+midiin = RtMidi::In.new
+
+puts "Available MIDI input ports"
+midiin.port_names.each_with_index{|name,index| puts "  ##{index+1}: #{name}" }
+
+midiin.set_callback do |byte1, byte2, byte3|  
+  puts "#{byte1} #{byte2} #{byte3}"
+end
+
+puts "Listening for MIDI messages..."
+puts "Ctrl+C to exit"
+
+midiin.open_port(1)
+
+sleep # prevent Ruby from exiting immediately
